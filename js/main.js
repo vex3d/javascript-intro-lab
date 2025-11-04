bd50a2140859c68d0d23962f1ab6bc8b0ab80bed
vex3d <balogunmolly@gmail.com>
2025-11-04 11:03:05 +0000

    before challenge 2

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@ document.getElementById("myHeading").innerHTML = "Watch Gachiakuta";
 //overwrites html -> orginially 'my demo site'
 document.querySelector('nav ul li a').setAttribute('href', 'currentPage');
 /*refactored version attempt
-- add all the colours to a class
-class Color {
-    constructor(name){ <-always add a contructor method
-        this.name = name;
-    }
+(()=>
+{
+    const colourButtons = document.querySelector()
+    const colourChange = ((ev)=>{
+    const colourPicked = ev.target.classList[0]+"back"
+    doucment.body.className = colourPicked==="resetBack" ? "" : colourPicked    
+    })
+colourButtons.forEach((btn)=>{
+    btn.eventlistener("click", colourChange)
 }
-
-
+}
+)()
 */
 
 //normal version
@@ -25,3 +29,31 @@ document.querySelector(".blue").addEventListener('click', function(ev){
 document.querySelector(".reset").addEventListener('click', function(ev){
     document.body.removeAttribute('class');
 })
+
+//rotating image
+//document.getElementById('myImages').addEventListener('click', imgRotate)
+let imageAr = [
+    'images/view1.jpg',
+    'images/view2.jpg',
+    'images/view3.jpg',
+    'images/fnaf-foxy-jumpscare.gif',
+    'images/view4.jpg',
+    'images/view5.jpg',
+    'images/view6.jpg',
+    'images/view7.jpeg',
+    'images/view67.jpeg',
+    'images/among-us-twerk.gif'
+  ];
+  
+  let currentIndex = 0;
+  
+  function chgImage() {
+    currentIndex++;
+    if (currentIndex >= imageAr.length) {
+      currentIndex = 0;
+    }
+    document.getElementById('myImages').setAttribute('src', imageAr[currentIndex]);
+  }
+  
+  setInterval(chgImage, 2000);
+
